e2788c506733c7be4a7134480bb8357128c11321
Huzefa1413 <79566904+Huzefa1413@users.noreply.github.com>
2022-10-19 09:59:52 +0500

    Home Page Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./components/screens/HomeScreen";
 import {Container} from 'react-bootstrap'
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
     <Header />
     <main>
       <Container className="py-3">
-        <h1>Welcome to ProShop</h1>
+        <HomeScreen />
       </Container>
     </main>
     <Footer />
