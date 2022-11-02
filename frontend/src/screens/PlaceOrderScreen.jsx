09279794361200965218eb7a83dd350c5c39b6be
Huzefa1413 <79566904+Huzefa1413@users.noreply.github.com>
2022-11-02 19:50:27 +0500

    Order Controller & Route

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Row, Col, ListGroup, Image, Card, ListGroupItem } from 'react-bootstrap'
+import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import CheckoutSteps from '../components/CheckoutSteps'
