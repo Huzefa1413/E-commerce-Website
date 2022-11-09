67bf358e9923bed3327b89889601f1626e2e95b6
Huzefa1413 <79566904+Huzefa1413@users.noreply.github.com>
2022-11-09 18:52:06 +0500

    Admin Product List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen.jsx";
 import UserEditScreen from "./screens/UserEditScreen.jsx";
+import ProductListScreen from "./screens/ProductListScreen.jsx";
 
 import { Container } from 'react-bootstrap'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -37,6 +38,8 @@ const App = () => {
             <Route path='/order/:id' element={<OrderScreen />} />
             <Route path='/admin/userlist' element={<UserListScreen />} />
             <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
+            <Route path='/admin/productlist' element={<ProductListScreen />} />
+
           </Routes>
         </Container>
       </main>
