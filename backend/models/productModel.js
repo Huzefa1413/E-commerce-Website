9a71746792e50a8369b4da39adb61ab25678285c
Huzefa1413 <79566904+Huzefa1413@users.noreply.github.com>
2022-11-16 21:49:57 +0500

    Create Review Endpoint

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -13,10 +13,17 @@ const reviewSchema = mongoose.Schema(
         comment: {
             type: String,
             required: true
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
         }
-    }, {
-    timestamps: true
-})
+    },
+    {
+        timestamps: true
+    }
+)
 
 const productSchema = mongoose.Schema(
     {
