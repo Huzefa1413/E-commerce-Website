a511dc212ecf7d08bd230868ecdcf2d344e2791f
Huzefa1413 <79566904+Huzefa1413@users.noreply.github.com>
2022-11-18 16:45:06 +0500

    Product Pagination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,6 +29,8 @@ const App = () => {
           <Routes>
             <Route path='/' element={<HomeScreen />} exact />
             <Route path='/search/:keyword' element={<HomeScreen />} exact />
+            <Route path='/page/:pageNumber' element={<HomeScreen />} exact />
+            <Route path='/search/:keyword/page/:pageNumber' element={<HomeScreen />} exact />
             <Route path='/product/:id' element={<ProductScreen />} />
             <Route path='/cart' element={<CartScreen />} />
             <Route path='/cart/:id' element={<CartScreen />} />
@@ -41,8 +43,9 @@ const App = () => {
             <Route path='/order/:id' element={<OrderScreen />} />
             <Route path='/admin/userlist' element={<UserListScreen />} />
             <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
-            <Route path='/admin/productlist' element={<ProductListScreen />} />
+            <Route path='/admin/productlist' element={<ProductListScreen />} exact />
             <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
+            <Route path='/admin/productlist/:pageNumber' element={<ProductListScreen />} exact />
             <Route path='/admin/orderlist' element={<OrderListScreen />} />
 
           </Routes>
