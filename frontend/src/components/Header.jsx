a1f5c5a2857d8e79e7961f1d943aa7d80427bb50
Huzefa1413 <79566904+Huzefa1413@users.noreply.github.com>
2022-11-24 17:47:21 +0500

    Edited Name

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
         <header> <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
             <Container>
                 <LinkContainer to="/">
-                    <Navbar.Brand>Huzefa's Ecommerce</Navbar.Brand>
+                    <Navbar.Brand>Huzefa's Ecommerce Site</Navbar.Brand>
                 </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
